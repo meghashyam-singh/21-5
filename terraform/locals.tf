23a73a00bc8f4d71dcf7402c6135e255fa107143
megha <megha@gmail.com>
2026-05-22 01:58:34 +0530

    21-5

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,4 @@
 locals {
     ami_id = data.aws_ami.roboshop.id
-    instance_type = var.environment == dev ? "t3.micro" : "t3.medium"
+    instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium"
 }
