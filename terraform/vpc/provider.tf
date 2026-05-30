eef2219fc8d80d5afb6e8a1715a2beb626ae1e3e
megha <megha@gmail.com>
2026-05-31 03:20:13 +0530

    tf

---
+++ terraform/vpc/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.46.0"
+    }
+  }
+  backend "s3" {
+    bucket = "21-05-bucket"
+    region = "us-east-1"
+    key = "roboshop/dev/sg"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1a"
+}
