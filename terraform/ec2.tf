5d5f0c71958510fb399cb287d746124fe428cce0
megha <megha@gmail.com>
2026-05-30 19:16:44 +0530

    tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "roboshop_instances" {
     ami = local.ami_id
     instance_type = local.instance_type
     subnet_id = aws_subnet.database[count.index % length(var.az)].id
-    vpc_security_group_ids = ["aws_security_group.roboshop_sg.id"]
+    vpc_security_group_ids = [aws_security_group.roboshop_sg.id]
     tags = {
         Name = "${local.common_name}-${var.instances[count.index]}"
     }
