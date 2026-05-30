dda0e0c5c5662f94f786d54f7fe05527ab827abe
megha <megha@gmail.com>
2026-05-31 04:21:53 +0530

    tf

--- terraform/vpc/parameter.tf
+++ terraform/vpc/parameter.tf
@@ -4,20 +4,20 @@ resource "aws_ssm_parameter" "vpc_id" {
     value = aws_vpc.roboshop_vpc.id
 }
 
-# resource "aws_ssm_parameter" "public_subnet_ids" {
-#     name = "${var.project}/${var.environment}/public_subnet_ids"
-#     type = "StringList"
-#     value = aws_subnet.public[*].id
-# }
+resource "aws_ssm_parameter" "public_subnet_ids" {
+    name = "${var.project}/${var.environment}/public_subnet_ids"
+    type = "StringList"
+    value = join(",", aws_subnet.public[*].id)
+}
 
-# resource "aws_ssm_parameter" "private_subnet_ids" {
-#     name = "${var.project}/${var.environment}/private_subnet_ids"
-#     type = "StringList"
-#     value = aws_subnet.private[*].id
-# }
+resource "aws_ssm_parameter" "private_subnet_ids" {
+    name = "${var.project}/${var.environment}/private_subnet_ids"
+    type = "StringList"
+    value = join(",", aws_subnet.private[*].id)
+}
 
-# resource "aws_ssm_parameter" "database_subnet_ids" {
-#     name = "${var.project}/${var.environment}/database_subnet_ids"
-#     type = "StringList"
-#     value = aws_subnet.database[*].id
-# }
+resource "aws_ssm_parameter" "database_subnet_ids" {
+    name = "${var.project}/${var.environment}/database_subnet_ids"
+    type = "StringList"
+    value = join(",", aws_subnet.database[*].id)
+}
