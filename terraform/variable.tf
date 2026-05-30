ae8468d06b675edee9070a0969f5caf4912d6e6f
megha <megha@gmail.com>
2026-05-30 19:00:04 +0530

    tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -32,3 +32,8 @@ variable "database_subnet_cidr_block" {
     type = list(string)
     default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
+
+variable "ingress_ports" {
+    type = list(number)
+    default = [22, 80, 8080, 443]
+}
