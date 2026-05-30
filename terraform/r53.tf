2f343c66b8d88f721eb64ce71713039c9dc1f4d8
megha <megha@gmail.com>
2026-05-30 19:59:02 +0530

    tf

---
+++ terraform/r53.tf
@@ -0,0 +1,8 @@
+resource "aws_route53_record" "roboshop_records" {
+    zone_id = "Z07930191W09SXLUZFTL0"
+    count = length(var.instances)
+    type = "A"
+    ttl = 1
+    name = "${var.instances[count.index]}.${domain_name}"
+    records = [aws_instance.roboshop_instances[count.index].private_ip]
+}
